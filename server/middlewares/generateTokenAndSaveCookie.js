42c3bea02c72c8d2be1d9404c471fe166d04e292
zainabro753951 <zainabro886@gmail.com>
2025-08-19 14:57:40 -0700

    Backend Cookie storing fixed

--- server/middlewares/generateTokenAndSaveCookie.js
+++ server/middlewares/generateTokenAndSaveCookie.js
@@ -10,9 +10,9 @@ export const generateTokenAndSaveCookie = (data, rememberMe = '30d', res) => {
 
   res.cookie('token', token, {
     httpOnly: true, // Prevent JS access
-    secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
-    maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000, // 30d or 1h in ms
-    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production', // Required on Render
+    maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000, // 30d or 1h
+    sameSite: 'None', // ✅ cross-domain cookies require None
   })
 
   return token
